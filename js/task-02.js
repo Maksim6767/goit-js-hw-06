2f549595662a6828f2d5256e0018e596f52a2a8f
Maksim6767 <nikmaksim20@gmail.com>
2022-09-25 18:14:31 +0300

    Did task02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.getElementById('ingredients');
+const elements = ingredients.map(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+
+  return liEl;
+})
+
+ingredientsEl.append(...elements);
+
+console.log(elements);
