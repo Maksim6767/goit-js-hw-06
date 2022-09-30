866b116c8bc14e0a4655cc384e2d600c3ac556e1
Maksim6767 <nikmaksim20@gmail.com>
2022-09-30 22:18:27 +0300

    Did task09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
-function getRandomHexColor() {
+const body = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+btnEl.addEventListener("click", getRandomHexColor => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+
+  function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+});
